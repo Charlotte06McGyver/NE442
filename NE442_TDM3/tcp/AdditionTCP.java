64b513d681ca32f5a1eeffd939ff372d4917fda8
Charlotte06McGyver <114010923+Charlotte06McGyver@users.noreply.github.com>
2023-09-27 17:59:02 +0200

    Add files via upload

--- NE442_TDM3/tcp/AdditionTCP.java
+++ NE442_TDM3/tcp/AdditionTCP.java
@@ -33,49 +33,58 @@ public class AdditionTCP
         Socket socket = new Socket();
 
         // Connexion au serveur 
-        InetSocketAddress adrDest = new InetSocketAddress("192.168.130.150", 7500);
-        socket.connect(adrDest);        
-
-        // Attente de la reponse 
+        InetSocketAddress adrDest = new InetSocketAddress("localhost", 7500);
+        socket.connect(adrDest);  
+        
+        // Buffer de la reponse 
         byte[] bufR = new byte[2048];
+        
+    	//Reception
+    	InputStream is = socket.getInputStream();
+    	int lenBufR = is.read(bufR);
+
+        //Buffer du calcul incomplet
+        String bufCalInc = new String("");
+        //Buffer qui contiendra une seule question à traiter
+        String calcul = new String();
+        //Résultat du calcul
+        int addition = 0;
+        //Indice de fin de partie
         int fin = 0;
         
         while(fin == 0) {
-        	//Reception
-        	InputStream is = socket.getInputStream();
-        	int lenBufR = is.read(bufR);
+
         	if (lenBufR!=-1) {
         		
-        		String reponse = new String(bufR, 0 , lenBufR );
-        		System.out.println("Question: "+reponse);
-        		//indicateurs de début et fin d'operande
-        		int prem = 0;
-        		int der = 0;
         		
-        		while(!"+".equals(reponse.substring(der,der+1))) {
-        			//System.out.println("Caractere lu : " + reponse.substring(der,der+1));
-        			der++;
-        		}       		
-        		int operande1 = Integer.parseInt(reponse.substring(prem, der));
-    			System.out.println("Operande 1 : " + reponse.substring(prem,der));
-        		der ++;
-        		prem = der;
-        		while(!"=".equals(reponse.substring(der,der+1))) {
-        			//System.out.println("Caractere lu : " + reponse.substring(der,der+1));
-        			der++;
-        		}        		
-        		int operande2 = Integer.parseInt(reponse.substring(prem,der));
-    			System.out.println("Operande 2 : " + reponse.substring(prem,der));
-    			der ++;
-            
-        		//Preparation de la reponse
-        		int result = operande1 + operande2;
-        		byte[] bufM = new String(result +";").getBytes();
-    			System.out.println(operande1 + " + " +operande2+"=" + result);
+        		String message = new String(bufR, 0 , lenBufR );
+        		System.out.println("message recu : "+message);
+        		
+        		//On récupère la fin de l'ancienne question (s'il y en a) + nouvelle question
+        		String question = bufCalInc + message;
+        		
+        		//On récupère des caractères tant que l'on n'a pas une question entière (avec un ?)
+        		while(!question.contains("?")) {
+        			lenBufR = is.read(bufR);
+        			question += new String(bufR, 0 , lenBufR );
+        		}
+        		
+        		calcul = question.substring(0, question.indexOf("?")+1);
+        		bufCalInc = question.substring(question.indexOf("?")+1);
+        		
+              		
+        		System.out.println("Question: "+calcul);
+        		
+        		addition = Calcul(calcul);
+        		
+        		byte[] bufM = new String(addition +";").getBytes();
+    			
             
         		//Envoi de la reponse
         		OutputStream os = socket.getOutputStream();
         		os.write(bufM);
+        		
+        		lenBufR = is.read(bufR);
         	}
         	else {
         		fin = 1;
@@ -86,6 +95,34 @@ public class AdditionTCP
         socket.close();
         System.out.println("Arret du client .");
 
+    }
+    
+    public int Calcul(String message) {
+    	//indicateurs de début et fin d'operande
+		int prem = 0;
+		int der = 0;
+		
+		while(!"+".equals(message.substring(der,der+1))) {
+			//System.out.println("Caractere lu : " + reponse.substring(der,der+1));
+			der++;
+		}       		
+		int operande1 = Integer.parseInt(message.substring(prem, der));
+		System.out.println("Operande 1 : " + message.substring(prem,der));
+		der ++;
+		prem = der;
+		while(!"=".equals(message.substring(der,der+1))) {
+			//System.out.println("Caractere lu : " + reponse.substring(der,der+1));
+			der++;
+		}        		
+		int operande2 = Integer.parseInt(message.substring(prem,der));
+		System.out.println("Operande 2 : " + message.substring(prem,der));
+		der ++;
+    
+		//Preparation de la reponse
+		int result = operande1 + operande2;
+		System.out.println(operande1 + " + " +operande2+"=" + result);
+		return result;
+    	
     }
 }
 
